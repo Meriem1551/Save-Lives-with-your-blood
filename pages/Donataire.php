eeca9616c0aee31b3226e8e0a14dabe05ad3a5f3
Meriem1551 <meriem200409@gmail.com>
2024-03-29 11:20:25 +0100

    login

--- pages/Donataire.php
+++ pages/Donataire.php
@@ -1,10 +1,10 @@
 <!-- adding a user if sign in or check for his existance for log in -->
 <?php 
 //FUNCTIONS DEFINITION
-function isExiste($id, $sign_email){
-    $searchReq = "select * from donataire where email = ?";
+function isExiste($id, $email, $passw){
+    $searchReq = "select * from donataire where email = ?  and password=?";
     $stmt = mysqli_prepare($id, $searchReq);
-    mysqli_stmt_bind_param($stmt, "s", $sign_email);
+    mysqli_stmt_bind_param($stmt, "ss", $email, $passw);
     mysqli_stmt_execute($stmt);
     $searchRes = mysqli_stmt_get_result($stmt);
     if (mysqli_num_rows($searchRes)> 0) {
@@ -16,7 +16,7 @@ function isExiste($id, $sign_email){
 }
 
 function addUser($id, $sign_email,$sign_passw, $birthday, $typeBlood) { 
-    if(isExiste($id, $sign_email)) {
+    if(isExiste($id, $sign_email, $sign_passw)) {
         echo "<script>";
         echo "alert('This user is already existe. Try to login');";
         echo "window.location.href='../pages/login.html';";
@@ -50,6 +50,22 @@ if( $id = mysqli_connect("localhost:3308", "root","mysql2024") ) {
             $birthday = $_POST['birthday'];
             $typeBlood = $_POST['typeBlood'];
             addUser($id, $sign_email, $sign_passw, $birthday, $typeBlood);
+        }
+        else if(isset($_POST['login'])){
+            $login_email = $_POST['login_email'];
+            $login_passw = $_POST['login_passw'];
+            if(isExiste($id, $login_email, $login_passw)){
+                echo "<script>";
+                echo "alert('Log in successfully');";
+                echo "window.location.href='../pages/Donataire.html';";
+                echo "</script>";
+            }
+            else{
+                echo "<script>";
+                echo "alert('Incorrect email or password');";
+                echo "window.location.href='../pages/login.html';";
+                echo "</script>";
+            }
         }
     }
     else {
