fb04cd9b87e1900fd19ef5ed4e28af2d39e75b9f
Meriem1551 <meriem200409@gmail.com>
2024-04-05 21:25:15 +0100

    update display donataires

--- php/AdminPage.php
+++ php/AdminPage.php
@@ -6,18 +6,48 @@
     <link rel="icon" type="image/png" sizes="32x32" href="../assets/images/icons8-blood-drop-32.png">
     <link rel="stylesheet" href="../styles/adminPage.css">
     <title>Admin page</title>
-    <style>
-    .single_user1 {
-        display: block;
-        margin-bottom: 10px;
-        background-color:red;
-        color: white;
-    }
-    </style>
 </head>
 <body>
     <main>
         <button onclick="showSearchBloc()">search</button>
+        <button onclick="addDate()">New date</button>
+        <div id="add_date">
+            <h2>When is the next date for donation?</h2>
+            <form action="" method="post">
+                <label for="date">Next date:</label><br>
+                <input type="date" name="date" required>
+                <input type="submit" name="set_date" value="Add">
+            </form>
+        </div>
+        <?php
+            if( $id = mysqli_connect("localhost:3308", "root","mysql2024") ) {
+                if( $id_db = mysqli_select_db($id, "hope_lab") ) {
+                    if(isset($_POST['set_date'])) {
+                        $new_date = $_POST['date'];
+                        $req = "insert into dates values (?)";
+                        $stmt = mysqli_prepare($id, $req);
+                        mysqli_stmt_bind_param($stmt, 's', $new_date);
+                        mysqli_stmt_execute($stmt);
+                        if(mysqli_stmt_affected_rows($stmt) <= 0){
+                            echo "<script>";
+                            echo "alert('Can not add this date');";
+                            echo "</script>"; 
+                        }
+                        else{
+                            header("Location: ../php/AdminPage.php");
+                            exit;
+                        }
+                    }
+                }
+                else{
+                    die("Echec de connexion a la base");
+                }
+                mysqli_close($id);
+            } 
+            else {
+                die("Erreur de connection au serveur");
+            }
+        ?>
         <div id="search_box">
             <h2>Search for a Donataire: </h2><br/>
             <form action="" method="post">
@@ -40,7 +70,7 @@
         <?php
             if( $id = mysqli_connect("localhost:3308", "root","mysql2024") ) {
                 if( $id_db = mysqli_select_db($id, "hope_lab") ) {
-                    $get_data = "select * from users order by family_name"; 
+                    $get_data = "select * from users where isDonate = true order by family_name "; 
                     $result = mysqli_query($id, $get_data);
                     if(mysqli_num_rows($result) <= 0){
                         echo"Empty data base";
@@ -72,7 +102,7 @@
                         if (isset($_POST['search'])) {
                             $startDate = $_POST['first_date'];
                             $endDate = $_POST['second_date'];
-                            $req = "select * from users where donation_date between ? and ?";
+                            $req = "select * from users where isDonate = true and donation_date between ? and ? ";
                             $stmt = mysqli_prepare($id, $req);
                             mysqli_stmt_bind_param($stmt, 'ss', $startDate, $endDate);
                             mysqli_stmt_execute($stmt);
@@ -123,6 +153,9 @@
         }
         function hideSearchBloc(){
             document.getElementById('search_box').style.display = "none";
+        }
+        function addDate(){
+            document.getElementById('add_date').style.display = "block";
         }
     </script>
 </body>
