65d086a4716affaa6d99a8f8441c5983b189521c
Meriem1551 <meriem200409@gmail.com>
2024-04-06 16:55:32 +0100

    adding comment page

--- php/comments.php
+++ php/comments.php
@@ -20,11 +20,66 @@
     </header>
     <!-- comments -->
     <!-- button to donation request -->
-    
+    <main>
+        <?php
+            if($id = mysqli_connect("localhost:3308", "root", "mysql2024")){
+                if($id_db = mysqli_select_db($id, "hope_lab")){
+                    if(isset( $_POST['post'])){
+                        $email = $_POST['email'];
+                        $comment = $_POST['comment'];
+                        $addComm = "insert into comments values(?,?)";
+                        $stmt = mysqli_prepare($id, $addComm);
+                        mysqli_stmt_bind_param($stmt, 'ss', $email, $comment);
+                        mysqli_stmt_execute($stmt);
+                        if(mysqli_stmt_affected_rows($stmt) <= 0){
+                            echo "<script>";
+                            echo "alert('Can not add this comment try later');";
+                            echo "</script>";   
+                        }
+                        else{
+                            $req = "select * from comments";
+                            $res =  mysqli_query($id,$req);
+                                while ($row = mysqli_fetch_assoc($res)) {
+                                echo"<div id='comment'>";
+                                    echo"<div id='comment'>".$row['email']."</div>";
+                                    echo"<div id='comment'>".$row['comment']."</div>";
+                                echo"</div>";
+                            }
+                        }
+                    }
+                }
+                else{
+                    die("Echec de connecter a cette base");
+                }
+                mysqli_close($id);
+            }
+            else{
+                die("Echec de connecter e ce serveur");
+            }
+        
+        ?>
+        <button onclick="postComment()">COMMENT NOW</button>
+        <div id="comments_box" style="display:none;">
+        <form action="" method="post">
+            <label for="email">Your email:</label><br>
+            <input type="email" name="email" required><br>
+            <label for="comment">Your comment :</label><br>
+            <textarea id="textArea" name="comment"></textarea><br>
+            <input type="submit" name="post" value="POST">
+        </form>
+        <button onclick="hideComment()">Discard</button>
+        </div>
+    </main>
     <!-- textarea for comment-->
     <!-- button to submit the comment -->
-    <!-- a div for comment contains email the comment, delte button i'll add the php so just add the div, -->
-    <a href="../php/Donerequest.php">Donate</a>
-
+    
+            <script>
+                function postComment(){
+                    document.getElementById('comments_box').style.display ="block";
+                }
+                function hideComment(){
+                    document.getElementById('comments_box').style.display ="none";
+                }
+            </script>
 </body>
 </html>
