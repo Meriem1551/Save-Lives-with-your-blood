7e48552558cf0ee9371281a8a716155cc46e647b
Meriem1551 <meriem200409@gmail.com>
2024-04-13 14:50:44 +0100

    removing statement

--- php/forgetpw.php
+++ php/forgetpw.php
@@ -1,12 +1,12 @@
 <?php
 function isExiste($id, $email){
-    $userInfo = "select * from users where email = ?"; 
-    $stmt = mysqli_prepare($id, $userInfo);
-    mysqli_stmt_bind_param($stmt, 's', $email);
-    mysqli_stmt_execute($stmt);
-    $GetUserInfo = mysqli_stmt_get_result($stmt);  
-    
-    if(mysqli_num_rows($GetUserInfo) > 0){
+    $userInfo = "select * from users where email = '$email' "; 
+    // $stmt = mysqli_prepare($id, $userInfo);
+    // mysqli_stmt_bind_param($stmt, 's', $email);
+    // mysqli_stmt_execute($stmt);
+    // $GetUserInfo = mysqli_stmt_get_result($stmt);  
+    $sql = mysqli_query($id, $userInfo);
+    if(mysqli_num_rows($sql) > 0){
         return true;
     }else{
         return false;
@@ -16,10 +16,11 @@ function isExiste($id, $email){
         $your_email = $_POST['your_email'];
         $new_passw = $_POST['new_passw'];
         if(isExiste($id, $your_email)){
-            $request = "update users set  password = '$new_passw' where email=?";
-            $stmt = mysqli_prepare($id,$request);
-            mysqli_stmt_bind_param($stmt,"s",$your_email);
-            if(mysqli_stmt_execute($stmt)) {
+            $request = "update users set  password= '$new_passw' where email='$your_email' ";
+            $sql = mysqli_query($id, $request);
+            // $stmt = mysqli_prepare($id,$request);
+            // mysqli_stmt_bind_param($stmt,"s",$your_email);
+            if($sql) {
                 echo "<script>alert('Updated password successfully. Log in now');window.location.href='../pages/login.html';</script>";  
             }
             else{
